dfccaf8b4f6f689193bd7ffddd5272cd01096b58
ToniR90 <romy90.t@gmail.com>
2025-03-20 11:51:11 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(0);
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+        List<Integer> list2 = new ArrayList<>();
+
+        ListIterator<Integer> iterator = list1.listIterator(list1.size());
+
+        while(iterator.hasPrevious()){
+            list2.add(iterator.previous());
+        }
+
+        System.out.println("First List: " + list1);
+        System.out.println("Second List: " + list2);
     }
 }
